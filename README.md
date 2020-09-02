f72c3ec532a7d661976ffe76150fdbb42e43a8a1
Anthony Bishopric <git@anthonybishopric.com>
2020-09-01 20:06:30 -0700

    statically serve

--- README.md
+++
@@ -1,8 +0,0 @@
-# Interval Playlist Generator
-
-TODOs:
-
-* Render spotify artwork as icons over intervals.
-* Save playlist to Spotify
-* Custom workout builder
-
---
+++ precache-manifest.5ec3c85dab75cf41d0b112e977887905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d443be8eebcb79412e272d3f8a22721e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "723b082c6b6daac4f5c8",
+    "url": "/static/css/main.b6fad0b2.chunk.css"
+  },
+  {
+    "revision": "d3a49ada58fb3a3974ad",
+    "url": "/static/js/2.d5942291.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/static/js/2.d5942291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "723b082c6b6daac4f5c8",
+    "url": "/static/js/main.bd1370d9.chunk.js"
+  },
+  {
+    "revision": "d779fbdab75d5c4b696a",
+    "url": "/static/js/runtime-main.bcbcf8ee.js"
+  }
+]);
